94d9b736d9b1b94b42eefada7d2340135f0cee78
Angela Enriquez <anenriqu@outlook.com>
2019-10-20 13:05:35 +0200

    setup: Add setupfile

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ropod_rosbag_processing',
+      version='1.0',
+      description='Processing of rosbag files',
+      package_dir={'': '.'}
+      )
