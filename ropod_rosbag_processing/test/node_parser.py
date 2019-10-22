29d3f9db75f6272a5b12569e32c25ae3a16450de
emassey2 <emassey2@wisc.edu>
2019-10-22 16:18:53 +0200

    seperating out functionality

--- ropod_rosbag_processing/test/node_parser.py
+++ ropod_rosbag_processing/test/node_parser.py
@@ -2,21 +2,10 @@ from ropod_rosbag_processing.utils.utils import load_yaml
 from ropod_rosbag_processing.graph.node import TravelNode
 
 
-def get_travel_nodes(nodes_dict):
-    nodes = list()
-    for name, pose in nodes_dict.items():
-        travel_node = TravelNode.from_dict({'name': name,
-                                            'pose': pose})
-        print(travel_node)
-        nodes.append(travel_node)
-
-    return nodes
-
-
 if __name__ == '__main__':
     config = "../config/config.yaml"
     config_params = load_yaml(config)
     nodes_dict = config_params.get('nodes')
-    nodes = get_travel_nodes(nodes_dict)
+    nodes = TravelNode.get_travel_nodes(nodes_dict)
 
 
