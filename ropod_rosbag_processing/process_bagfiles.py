50987d81ec95cd920328f1e73b510fee7b0c913e
Angela Enriquez <anenriqu@outlook.com>
2019-10-23 15:57:59 +0200

    process_bagfiles: Use TravelLogger to parse bagfiles

--- ropod_rosbag_processing/process_bagfiles.py
+++ ropod_rosbag_processing/process_bagfiles.py
@@ -2,8 +2,12 @@ from ropod_rosbag_processing.utils.utils import load_yaml
 import os
 from ropod_rosbag_processing.graph.node import TravelNode
 from ropod_rosbag_processing.utils.get_files import get_bagfiles
+from ropod_rosbag_processing.graph.travel_logger import TravelLogger
+import rosbag
+from ropod_rosbag_processing.pose import Pose
 
-TO_PROCESS_DIR = 'to_process/'
+
+TO_PROCESS_DIR = '../../input/'
 
 
 def get_config_files(path):
@@ -23,12 +27,38 @@ def parse_config_file(config_file):
     return nodes, output_dir
 
 
+def update_travel_logger(bagfile, travel_loggers):
+    seen_topics = {}
+
+    bag = rosbag.Bag(TO_PROCESS_DIR + bagfile)
+    for topic, msg, cur_time in bag.read_messages():
+        seen_topics[topic] = topic
+
+        if "/amcl_pose" in topic:
+            ros_pose = msg.pose.pose.position
+            cur_pose = Pose(ros_pose.x, ros_pose.y, ros_pose.z)
+            for travel_logger in travel_loggers:
+                travel_logger.event_update(cur_pose, cur_time)
+
+
 def process():
     config_files = get_config_files('config/')
     bagfiles = get_bagfiles(TO_PROCESS_DIR)
+    travel_loggers = list()
+    output_dirs = list()
     for config_file in config_files:
         nodes, output_dir = parse_config_file(config_file)
-        # Create TravelLogger object
+        travel_logger = TravelLogger(nodes)
+        travel_loggers.append(travel_logger)
+        output_dirs.append(output_dir)
+
+    for bagfile in bagfiles:
+        update_travel_logger(bagfile, travel_loggers)
+        for i, travel_logger in enumerate(travel_loggers):
+            print(travel_logger.get_history())
+            travel_logger.to_file(output_dirs[i], file_suffix=bagfile.replace('.bag', '.txt'))
+
+process()
 
     # TODO: Move files from TO_PROCESS_DIR to PROCESSED_DIR
 
