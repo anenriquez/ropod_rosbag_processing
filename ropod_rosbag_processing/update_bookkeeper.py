22e08fbac1901787bc353ea1ddf1fa8c79a5f63c
Angela <anenriqu@outlook.com>
2019-10-28 13:33:33 +0100

    Update path to workstation_dir

--- ropod_rosbag_processing/update_bookkeeper.py
+++ ropod_rosbag_processing/update_bookkeeper.py
@@ -3,11 +3,12 @@ from ropod_rosbag_processing.utils.get_files import add_bagfile_to_register
 
 
 # File with name of bagfiles that have been copied to the workstation
-bagfile_bookkepper = "bookkeeper.csv"
-WORKSTATION_DIR = 'home/ropod/guido_bags/'
+bagfile_bookkepper = "utils/bookkeeper.csv"
+WORKSTATION_DIR = '/home/ropod/guido_bags/'
 
 
 if __name__ == '__main__':
     bagfiles = get_bagfiles(WORKSTATION_DIR)
+    print(bagfiles)
     for bagfile in bagfiles:
         add_bagfile_to_register(bagfile_bookkepper, bagfile)
