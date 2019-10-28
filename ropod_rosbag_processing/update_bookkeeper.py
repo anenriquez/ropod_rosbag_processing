76fc4848565146a7946ef44889b34ebd30744df2
Angela Enriquez <anenriqu@outlook.com>
2019-10-28 13:04:43 +0100

    Add script to update bookkeeper

---
+++ ropod_rosbag_processing/update_bookkeeper.py
@@ -0,0 +1,13 @@
+from ropod_rosbag_processing.utils.get_files import get_bagfiles
+from ropod_rosbag_processing.utils.get_files import add_bagfile_to_register
+
+
+# File with name of bagfiles that have been copied to the workstation
+bagfile_bookkepper = "bookkeeper.csv"
+WORKSTATION_DIR = 'home/ropod/guido_bags/'
+
+
+if __name__ == '__main__':
+    bagfiles = get_bagfiles(WORKSTATION_DIR)
+    for bagfile in bagfiles:
+        add_bagfile_to_register(bagfile_bookkepper, bagfile)
