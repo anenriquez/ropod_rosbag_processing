afdb3b22cc2cb54120510a9a3e2898ffebf1b250
Angela Enriquez <anenriqu@outlook.com>
2019-10-28 14:17:36 +0100

    run.sh: Add bash script to process bagfiles

---
+++ ropod_rosbag_processing/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python3 /home/ropod/ropod_rosbag_processing/ropod_rosbag_processing/merge_bag.py
+python3 /home/ropod/ropod_rosbag_processing/ropod_rosbag_processing/process_bagfiles.py
