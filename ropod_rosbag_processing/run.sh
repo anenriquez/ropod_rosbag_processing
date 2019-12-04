1fc3f8646b1a244d7a78a668b2ea9b2bb53d097c
Ángela Enríquez Gómez <anenriqu@outlook.com>
2019-12-04 12:49:36 +0100

    run.sh: Add results_generator script

--- ropod_rosbag_processing/run.sh
+++ ropod_rosbag_processing/run.sh
@@ -6,3 +6,4 @@ cd /home/ropod/ropod_rosbag_processing/ropod_rosbag_processing/ethan/travel_time
 ./cat_files.sh
 cd /home/ropod/ropod_rosbag_processing/ropod_rosbag_processing/angela/travel_time
 ./cat_files.sh
+python3 /home/ropod/ropod_rosbag_processing/ropod_rosbag_processing/results_generator.py
