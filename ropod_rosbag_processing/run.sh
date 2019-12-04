17415b579cddcbbcf05a62f38950a27fee94fadb
Ángela Enríquez Gómez <anenriqu@outlook.com>
2019-12-04 14:45:21 +0100

    run.sh: Add get_edge_summary script

--- ropod_rosbag_processing/run.sh
+++ ropod_rosbag_processing/run.sh
@@ -7,3 +7,4 @@ cd /home/ropod/ropod_rosbag_processing/ropod_rosbag_processing/ethan/travel_time
 cd /home/ropod/ropod_rosbag_processing/ropod_rosbag_processing/angela/travel_time
 ./cat_files.sh
 python3 /home/ropod/ropod_rosbag_processing/ropod_rosbag_processing/results_generator.py
+python3 /home/ropod/ropod_rosbag_processing/ropod_rosbag_processing/get_edge_summary.py
